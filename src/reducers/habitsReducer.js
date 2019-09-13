d4fdcb6645c2c9ee7157f11b679627ca62bfdb0f
Lili Boxer <ninaliboxer@gmail.com>
2019-09-13 13:07:31 -0700

    action fetchHabits created in reducer

--- src/reducers/habitsReducer.js
+++ src/reducers/habitsReducer.js
@@ -1,4 +1,4 @@
-import { ADD_HABIT } from '../actions/habitActions';
+import { ADD_HABIT, FETCH_HABITS } from '../actions/habitActions';
 
 const initialState = {
   habits: []
@@ -7,6 +7,8 @@ const initialState = {
 export default function habitsReducer(state = initialState, action) {
   switch(action.type) {
     case ADD_HABIT:
+      return { ...state, habits: [...state.habits, action.payload] };
+    case FETCH_HABITS:
       return { ...state, habits: [...state.habits, action.payload] };
     default:
       return state;
