60f770478f7d8ade6c543f8ade8b3be709eaf129
Lili Boxer <ninaliboxer@gmail.com>
2019-09-13 17:00:00 -0700

    brought headers into habitApi

--- src/services/habitApi.js
+++ src/services/habitApi.js
@@ -4,21 +4,27 @@ export const setToken = newToken => {
 };
 
 export const postHabit = (habit) => {
-  return fetch(`${process.env.API_URL}/api/v1/habits`), {
+  return fetch(`${process.env.API_URL}/api/v1/habits`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
     },
-    body: JSON.stringify(habit)
-  }
+    body: JSON.stringify({ habit })
+  })
     .then(res => {
       if(res.ok) return res.json();
-    });
+    })
+    .catch(console.log);
 };
 
 export const getHabits = () => {
-  return fetch(`${process.env.API_URL}/api/v1/habits`)
+  return fetch(`${process.env.API_URL}/api/v1/habits`, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    }
+  })
     .then(res => {
       if(!res.ok) throw 'Unable to get your habits';
       return res.json();
