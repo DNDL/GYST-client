4e6482ea3b4cee5c5c7e95ed595457a648b87ef9
Lili Boxer <ninaliboxer@gmail.com>
2019-09-16 09:54:30 -0700

    created a habitSelectors.js

--- src/selectors/habitSelectors.js
+++ src/selectors/habitSelectors.js
@@ -1,1 +1,7 @@
 export const getAllHabits = state => state.habits.habits;
+
+export const getHabit = (state, _id) => {
+  return getAllHabits(state).filter(habit => {
+    return habit._id === _id;
+  });
+};
