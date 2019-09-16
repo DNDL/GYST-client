4c75875919f7cd4c84bc10ae95552cdd41ff342a
Lili Boxer <ninaliboxer@gmail.com>
2019-09-16 09:51:35 -0700

    created a FormPage.js

---
+++ src/Pages/FormPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddHabit from '../components/Containers/AddHabit';
+import Nav from '../components/Nav/Nav';
+
+export default function FormPage() {
+  return (
+    <>
+      <AddHabit />
+      <Nav />
+    </>
+  );
+}
