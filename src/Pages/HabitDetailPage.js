4215c90b1b81b3815ea830bdf0ac6030af5befda
Lili Boxer <ninaliboxer@gmail.com>
2019-09-16 09:51:55 -0700

    created HabitDetailPage.js

---
+++ src/Pages/HabitDetailPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../components/Nav/Nav';
+import Habit from '../components/Containers/Habit';
+
+export default function HabitDetailPage() {
+  return (
+    <>
+      <Habit />
+      <Nav />
+    </>
+  );
+}
