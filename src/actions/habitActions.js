546fb96531ccc2ca87669cbc22a3c8cff2a8beba
Dirt Deodara <thecrowleaps@gmail.com>
2019-09-17 11:05:02 -0700

    removing  getHabit requiring id

--- src/actions/habitActions.js
+++ src/actions/habitActions.js
@@ -3,18 +3,18 @@ import { getAttemptsById } from '../selectors/habitSelectors';
 
 export const ADD_HABIT = 'ADD_HABIT';
 export const addHabit = habit => ({
-  type: ADD_HABIT,
+  type: ADD_HABIT, 
   payload: postHabit(habit)
 });
 
 export const FETCH_HABITS = 'FETCH_HABITS';
-export const fetchHabits = (id) => ({
+export const fetchHabits = () => ({
   type: FETCH_HABITS,
-  payload: getHabits(id)
+  payload: getHabits()
 });
 
 export const FETCH_ATTEMPTS_BY_ID = 'FETCH_ATTEMPTS_BY_ID';
-export const fetchAttemptsById = (habitId) => ({
+export const fetchAttemptsById = habitId => ({
   type: FETCH_ATTEMPTS_BY_ID,
   payload: getAttemptsById(habitId)
 });
