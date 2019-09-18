721bcfd68ada0b6430d43ff80a9ee2ffff53aef4
Lili Boxer <ninaliboxer@gmail.com>
2019-09-17 18:48:06 -0700

    habit container for habitDetail

--- src/components/Containers/Habit.js
+++ src/components/Containers/Habit.js
@@ -1,11 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import HabitDetailItem from '../Habit/HabitDetailItem';
-import { getHabitsByUserId } from '../../selectors/habitSelectors';
+import { getHabitById } from '../../selectors/habitSelectors';
 
-const mapStateToProps = (state, props) => ({
-  habit: getHabitsByUserId(state, props.match.params._id)
-});
+
+class Habit extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    why: PropTypes.string.isRequired
+  }
+
+  render() {
+    const { title, why } = this.props;
+    return (
+      <section>
+        <h3>{title}</h3>
+        <p>{why}</p>
+        <p>DATA VISUALIZATION</p>
+        <button>Edit</button>
+      </section>
+    );
+  }
+}
+
+const mapStateToProps = (state, props) => {
+  return {  habit: getHabitById(state, props.props.match.params._id)}
+}
 
 export default connect(
   mapStateToProps
-)(HabitDetailItem);
+)(Habit);
