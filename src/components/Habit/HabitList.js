3e5d9d5028641374a7a4bd6acb394124c3b37659
Lili Boxer <ninaliboxer@gmail.com>
2019-09-17 17:31:11 -0700

    remove console.logs

--- src/components/Habit/HabitList.js
+++ src/components/Habit/HabitList.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import HabitListItem from './HabitListItem';
-// import { getHabitsByUserId, getAttemptsById } from '../../selectors/habitSelectors';
 
 // TODO: increment button
 // TODO: display most recent attemps for progress visualization
 
 function HabitList(habits) {
-  console.log('in habitList', habits.habits);
   const habitElement = habits.habits.map(habit => (
     <li key={habit._id}>
       <HabitListItem habit={habit}  />
