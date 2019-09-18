092d312f1bdbfb702fef35808eba33c7a3f178df
Lili Boxer <ninaliboxer@gmail.com>
2019-09-17 17:31:45 -0700

    getHabitById, getAllAttempts

--- src/selectors/habitSelectors.js
+++ src/selectors/habitSelectors.js
@@ -1,8 +1,14 @@
 export const getAllHabits = state => state.userHabits.habits;
 
+export const getHabitById = (state, id) => {
+  return getAllHabits(state).filter(habit => {
+    return id === habit._id;
+  });
+};
+
 export const getAllAttempts = state => state.userHabits.attempts;
 
-export const getAttemptByHabitId = (state, habit) => {
+export const getAttemptsByHabitId = (state, habit) => {
   return state.userHabits.attempts
     .filter((attempt) => {
       return attempt.habit === habit._id;
