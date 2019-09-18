cb74df38d2eccc24c624dd738d1819973bd534cf
Lili Boxer <ninaliboxer@gmail.com>
2019-09-18 09:19:03 -0700

    increenting attempt functionality

--- src/components/Containers/HabitListItem.js
+++ src/components/Containers/HabitListItem.js
@@ -2,33 +2,43 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-// import { getAttemptByHabitId } from '../../selectors/habitSelectors';
+import { getAttemptsByHabitId } from '../../selectors/habitSelectors';
+import { addAttempt } from '../../actions/habitActions';
 
-let habit;
 
 class HabitListItem extends Component {
   static propTypes = {
     habit: PropTypes.object.isRequired,
-    // attempt: PropTypes.object
+    attempts: PropTypes.array,
+    handleClick: PropTypes.func.isRequired,
   };
   render() {
-    // const { attempt } = this.props;
-    habit = this.props.habit;
+    const { attempts, habit, handleClick } = this.props;
+
     return (
       <>
         <Link to={`/habitDetail/${habit._id}`}>
           <p style={{ color: habit.color }}>{habit.title}</p>
+          <p>{attempts.length}/{habit.goal}</p>
         </Link>
         {/* <progress value={attempt.progress} max={habit.goal}></progress> */}
-        <button>Done</button>
+        <button onClick={() => handleClick(habit._id)}>Done</button>
       </>
     );
   }
 }
 
-// const mapStateToProps = (state, habit) => {
-//   console.log('in map state', habit);
-//   return { attempt: getAttemptByHabitId(state, habit.habit._id) };
-// };
+const mapStateToProps = (state, props) => ({
+  attempts: getAttemptsByHabitId(state, props)
+});
 
-export default connect()(HabitListItem);
+const mapDispatchToProps = dispatch => ({
+  handleClick(habit) {
+    dispatch(addAttempt(habit));
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HabitListItem);
