bc5b8f869274f73c803e527b8d2a53b3fa2ca61d
Dan Hogan <hogan.daniel@hotmail.com>
2019-09-18 17:28:27 -0700

    decontruct habits from props

--- src/components/Habit/HabitList.js
+++ src/components/Habit/HabitList.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './habitList.css';
-// import { getHabitsByUserId, getAttemptsById } from '../../selectors/habitSelectors';
 import HabitListItem from '../Containers/HabitListItem';
 
 // TODO: increment button
 // TODO: display most recent attemps for progress visualization
 
-function HabitList(habits) {
-  const habitElement = habits.habits.map(habit => (
+function HabitList({ habits }) {
+  const habitElement = habits.map(habit => (
     <li key={habit._id} className={styles.li}>
-      <HabitListItem habit={habit}  />
+      <HabitListItem habit={habit} />
     </li>
   ));
 
