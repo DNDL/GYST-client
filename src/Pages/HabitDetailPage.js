858a89b2bb4a21df5f09f0582f789a4739ea0c09
Lili Boxer <ninaliboxer@gmail.com>
2019-09-18 22:21:19 -0700

    styling of user header

--- src/Pages/HabitDetailPage.js
+++ src/Pages/HabitDetailPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Nav from '../components/Nav/Nav';
 import Habit from '../components/Containers/Habit';
+import Logo from '../components/Logo/Logo';
 
 export default function HabitDetailPage(props) {
   return (
     <>
+      <Logo />
       <Habit props={props}/>
       <Nav />
     </>
